b77cbd6fc876860d0e445db65e8d1517f1487198
Amir Khan <amir.khan.sch@gmail.com>
2023-04-24 04:39:14 +0100

    changes to app.js for the requirement given

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 export default function () {
+    const [employees, setEmployees] = useState([]);
 
     async function getEmployees() {
         return fetch("/employees").then(response => response.json());
@@ -22,7 +23,43 @@ export default function () {
         });
     }
 
+    useEffect(() => {
+        async function fetchEmployees() {
+            const data = await getEmployees();
+            setEmployees(data);
+        }
+        fetchEmployees();
+    }, []);
+
+    const handleAddEmployee = async () => {
+        const name = window.prompt('Enter employee name:');
+        const value = parseInt(window.prompt('Enter employee value:'));
+        await createEmployee(name, value);
+        setEmployees([...employees, { name, value }]);
+    }
+
+    const handleEditEmployee = async (index) => {
+        const { name, value } = employees[index];
+        const newName = window.prompt('Enter new name:', name);
+        const newValue = parseInt(window.prompt('Enter new value:', value));
+        await updateEmployee(newName, newValue);
+        const updatedEmployees = [...employees];
+        updatedEmployees[index].name = newName;
+        updatedEmployees[index].value = newValue;
+        setEmployees(updatedEmployees);
+    }
+
     return (
-        <div>Complete your app here</div>
+        <div>
+            <button onClick={handleAddEmployee}>Add Employee</button>
+            <ul>
+                {employees.map((employee, index) =>
+                    <li key={index}>
+                        {employee.name} - {employee.value}
+                        <button onClick={() => handleEditEmployee(index)}>Edit</button>
+                    </li>
+                )}
+            </ul>
+        </div>
     );
 }
